71d436b5e64b890a9b5b61017641c4ee9a63e38e
Filip Johansson <filip@aiwizo.com>
2024-02-08 09:44:34 +0100

    fix thursday bug

--- main.js
+++ main.js
@@ -13,10 +13,11 @@
   const today = new Date().getDay();
   const pageTitle = document.getElementById("day-name");
 
-  pageTitle.innerHTML = getNameOf(today + 3);
+  pageTitle.innerHTML = getNameOf((today + 3) % 7);
 
   [2, 3, 4, 5, 6, 7].map((n) => {
     const element = document.getElementById(`x${n}`);
+
     const lillDay = (today + 3 * n) % 7;
 
     if (lillDay === 6) {
